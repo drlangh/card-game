b77f3fea0477f2bf678bf18cf3843cad41d61b4f
DavidLangarica <a01708936@tec.mx>
2024-11-26 20:39:48 -0700

    nextconfig edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    // models available: 'gemini-1.5-pro', 'gemini-1.5-flash-8b', 'gemini-1.5-flash', 'aqa'
-    GEMINI_MODEL: 'gemini-1.5-flash-8b',
-    GEMINI_API_KEY: 'AIzaSyDmQjsg_wA33t3pCBupxSgzoXAYzouE3dU',
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
