8ca3022a5dc39259b88bf04d15ce8014b80bdf3f
DavidLangarica <a01708936@tec.mx>
2024-11-26 21:07:44 -0700

    env fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
