ef08e528d1afa04ec4f95729dc2e244e06006d87
drlangh <a01708936@tec.mx>
2024-11-28 21:23:56 -0700

    add uri to gemini.js

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
 const nextConfig = {
   env: {
     GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
-    FILE_DISPLAY_NAME: process.env.NEXT_PUBLIC_FILE_DISPLAY_NAME,
-    FILE_URI: process.env.NEXT_PUBLIC_FILE_URI,
-    MIME_TYPE: process.env.NEXT_PUBLIC_MIME_TYPE,
   },
   webpack(config) {
     config.module.rules.push({
--- src/api/gemini.js
+++ src/api/gemini.js
@@ -1,7 +1,11 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 const promptConfig = require('@/json/prompt.json'); // Renamed to avoid shadowing
 
-const maxTries = 3;
+const MAX_TRIES = 3;
+const FILE_URI =
+  'https://generativelanguage.googleapis.com/v1beta/files/tuxt9yxica6c';
+const MIME_TYPE = 'application/pdf';
+
 const genAI = new GoogleGenerativeAI(
   process.env.NEXT_PUBLIC_GEMINI_API_KEY
 );
@@ -16,7 +20,7 @@ export async function getCard(age, category) {
     .replace(/{age}/g, age)
     .replace(/{category}/g, category.name);
 
-  for (let tries = 0; tries < maxTries; tries++) {
+  for (let tries = 0; tries < MAX_TRIES; tries++) {
     try {
       const completion = await model.generateContent(prompt1);
       const assistantReply = completion.response.text().trim();
@@ -33,7 +37,7 @@ export async function getCard(age, category) {
     } catch (error) {
       console.error('Error generating card:', error);
 
-      if (tries === maxTries - 1) {
+      if (tries === MAX_TRIES - 1) {
         cardData = {
           content:
             error instanceof SyntaxError
@@ -60,13 +64,13 @@ export async function getMoreInformation(content, age, category) {
     .replace(/{category}/g, category.name)
     .replace(/{content}/g, content);
 
-  for (let tries = 0; tries < maxTries; tries++) {
+  for (let tries = 0; tries < MAX_TRIES; tries++) {
     try {
       const completion = await model.generateContent([
         {
           fileData: {
-            mimeType: process.env.NEXT_PUBLIC_MIME_TYPE,
-            fileUri: process.env.NEXT_PUBLIC_FILE_URI,
+            mimeType: MIME_TYPE,
+            fileUri: FILE_URI,
           },
         },
         {
@@ -79,10 +83,10 @@ export async function getMoreInformation(content, age, category) {
     } catch (error) {
       console.error('Error generating more information:', error);
 
-      if (tries === maxTries - 1) {
+      if (tries === MAX_TRIES - 1) {
         moreInformation =
           error instanceof SyntaxError
-            ? 'Everyone deserves to feel safe and respected. Learn how you can help.'
+            ? 'Know more about this topic by visiting the link below.'
             : 'An error occurred while generating more information. Please try again.';
 
         return moreInformation;
