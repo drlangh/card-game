6b20ef7f8683acb9f635a4f9c172c27039d5e6a2
drlangh <a01708936@tec.mx>
2024-12-06 12:21:21 -0700

    add day validation

--- src/app/cards/page.js
+++ src/app/cards/page.js
@@ -6,7 +6,14 @@ export default async function Cards() {
 
   if (!fileUri) {
     fileUri = await uploadFile();
+  } else {
+    const lastModified = await fetch(fileUri).then(
+      (res) => new Date(res.headers.get('last-modified'))
+    );
+    const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
+    if (lastModified < oneDayAgo) {
+      fileUri = await uploadFile();
+    }
   }
-
   return <CardClient fileUri={fileUri} />;
 }
