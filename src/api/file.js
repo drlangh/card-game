74b7196b57817001daa89fd9080660c7b8527ed2
drlangh <a01708936@tec.mx>
2024-12-11 17:41:30 -0700

    add timeOfUpload

--- src/api/file.js
+++ src/api/file.js
@@ -1,23 +1,19 @@
 'use server';
-import { GoogleGenerativeAI } from '@google/generative-ai';
 import { GoogleAIFileManager } from '@google/generative-ai/server';
 
-// Initialize GoogleGenerativeAI with your NEXT_PUBLIC_GEMINI_API_KEY.
-const genAI = new GoogleGenerativeAI(
-  process.env.NEXT_PUBLIC_GEMINI_API_KEY
-);
 // Initialize GoogleAIFileManager with your NEXT_PUBLIC_GEMINI_API_KEY.
 const fileManager = new GoogleAIFileManager(
   process.env.NEXT_PUBLIC_GEMINI_API_KEY
 );
 
 export default async function uploadFile() {
+  const timeOfUpload = new Date().toISOString();
   // Upload the file and specify a display name.
   const uploadResponse = await fileManager.uploadFile(
     './R41_Shift_2020_Practitioners_Framework_Engaging_Male-Oriented_Settings.pdf',
     {
       mimeType: 'application/pdf',
-      displayName: 'CardGame PDF',
+      displayName: 'CardGame PDF ' + timeOfUpload,
     }
   );
 
